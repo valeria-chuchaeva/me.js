638a19b873a55aa9b871f22310074e2cd1f3b6a7
Valeria Chuchaeva <cucaevavaleria8@gmail.com>
2026-05-08 21:06:10 +0300

    Create It-is-I.js

---
+++ It-is-I.js
@@ -0,0 +1,16 @@
+let name = "Валерия";
+let age = 19;
+let university = "ННГАСУ";
+let dream = "Стажировка в T1 как фронтенд-разработчик";
+
+console.log("Меня зовут " + name);
+console.log("Мне " + age + " лет");
+console.log("Я учусь в " + university);
+console.log("Моя мечта: " + dream);
+
+function myMotivation() {
+    console.log("Я не знала GitHub и JS 2 дня назад, а сегодня уже пишу код!");
+    console.log("Я ничего не боюсь и готова учиться");
+}
+
+myMotivation();
